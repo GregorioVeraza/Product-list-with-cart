cf8b9743c7556cb1c35d6a7babd218e0d0793aaf
GregorioVeraza <168499718+GregorioVeraza@users.noreply.github.com>
2025-08-29 02:27:09 -0300

    Added all the functions

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const product = [
-    { id:0 ,name: 'Waffle', price: 6.5 },
-    { id:1 ,name: 'Creme Brulee', price: 7.0 },
-    { id:2 ,name: 'Macaron', price: 8.0 },
-    { id:3 ,name: 'Tiramisu', price: 5.5 },
-    { id:4 ,name: 'Baklava', price: 4.0 },
-    { id:5 ,name: 'Pie', price: 5.0 },
-    { id:6 ,name: 'Cake', price: 4.5 },
-    { id:7 ,name: 'Brownie', price: 5.5 },
-    { id:8 ,name: 'Panna Cotta', price: 6.5 }
+    { id:0 ,name: 'Waffle', price: 6.5, img: "./assets/images/image-waffle-desktop.jpg" },
+    { id:1 ,name: 'Creme Brulee', price: 7.0, img:"./assets/images/image-creme-brulee-desktop.jpg" },
+    { id:2 ,name: 'Macaron', price: 8.0, img:"./assets/images/image-macaron-desktop.jpg" },
+    { id:3 ,name: 'Tiramisu', price: 5.5, img:"./assets/images/image-tiramisu-desktop.jpg" },
+    { id:4 ,name: 'Baklava', price: 4.0, img:"./assets/images/image-baklava-desktop.jpg" },
+    { id:5 ,name: 'Pie', price: 5.0, img:"./assets/images/image-meringue-desktop.jpg" },
+    { id:6 ,name: 'Cake', price: 4.5, img:"./assets/images/image-cake-desktop.jpg" },
+    { id:7 ,name: 'Brownie', price: 5.5, img:"./assets/images/image-brownie-desktop.jpg" },
+    { id:8 ,name: 'Panna Cotta', price: 6.5, img:"./assets/images/image-panna-cotta-desktop.jpg"}
 ]
 
 let cart = [];
@@ -58,6 +58,7 @@ function incrementFromCart(id) {
     const i = cart.indexOf(cart.find(o => o.id === id));
     cart[i].quantity += 1;
     document.getElementsByClassName("prices")[i].children[0].textContent = cart[i].quantity+"x";
+    document.getElementsByClassName("prices")[i].children[2].textContent = "$"+cart[i].quantity*product[id].price;
     document.getElementsByClassName("counter")[id].textContent = cart[i].quantity;
     modifarTotal();
 }
@@ -67,17 +68,12 @@ function decrementFromCart(id) {
     if (cart[i].quantity === 0) return;
     cart[i].quantity -= 1;
     document.getElementsByClassName("prices")[i].children[0].textContent = cart[i].quantity+"x";
+    document.getElementsByClassName("prices")[i].children[2].textContent = "$"+cart[i].quantity*product[id].price;
     document.getElementsByClassName("counter")[id].textContent = cart[i].quantity;
     modifarTotal();
 }
 
-function confirmOrder() {
-    
-}
 
-function newOrder() {
-    
-}
 
 function deleteProduct(pos) {
     const id = cart[pos].id;
@@ -88,38 +84,48 @@ function deleteProduct(pos) {
 
     products[id].getElementsByTagName("button")[0].style.display = "flex";
     products[id].getElementsByClassName("quantity-controls")[0].classList.add("oculto");
+    document.getElementsByClassName("counter")[id].textContent=0;
+}
+
+function divPrices(id, pos){
+    const div = document.createElement("div");
+    div.appendChild(addFirstParagraph(pos));
+    div.appendChild(addSecondParagraph(id));
+    div.classList.add("prices");
+    return div;
+}
+
+function divParagraph(id, pos){
+    const containerParagraph = document.createElement("div");
+    containerParagraph.classList.add("paragraph-details");
+
+    
+    containerParagraph.appendChild(addTittle(id));
+
+    const containerPrices = divPrices(id, pos);
+    
+
+    
+    
+    
+    containerParagraph.appendChild(containerPrices);
+    return containerParagraph;
 }
 
 function detailsProduct(id, pos) {
     const container = document.createElement("div");
     container.classList.add("details");
-    const containerParagraph = document.createElement("div");
 
-    const containerName = document.createElement("div");
-    const nameProduct = document.createElement("h4");
-    nameProduct.textContent = product[id].name;
-    containerName.appendChild(nameProduct);
-    containerParagraph.appendChild(containerName);
-
-    const containerPrices = document.createElement("div");
-
-    const firstParagraph = document.createElement("p");
-    firstParagraph.textContent = cart[pos].quantity +"x";
-
-    const secondParagraph = document.createElement("p");
-    secondParagraph.textContent = "@ "+product[id].price;
+    const containerParagraph = divParagraph(id, pos);
 
     const thirdParagraph = document.createElement("p");
     thirdParagraph.textContent = "$"+cart[pos].quantity*product[id].price;
+    containerParagraph.lastChild.appendChild(thirdParagraph);
 
-    containerPrices.appendChild(firstParagraph);
-    containerPrices.appendChild(secondParagraph);
-    containerPrices.appendChild(thirdParagraph);
-    containerPrices.classList.add("prices");
-    containerParagraph.appendChild(containerPrices);
     container.appendChild(containerParagraph);
 
     const buttonDelete = document.createElement("button");
+    buttonDelete.classList.add("delete");
     buttonDelete.onclick = function() { deleteProduct(pos); };
     const imgDelete = document.createElement("img");
     imgDelete.src = "./assets/images/icon-remove-item.svg";
@@ -128,4 +134,61 @@ function detailsProduct(id, pos) {
     container.appendChild(buttonDelete);
 
     return container;
+}
+
+function addTittle(id){
+    const nameProduct = document.createElement("h4");
+    nameProduct.textContent = product[id].name;
+    nameProduct.style.marginBottom = "0px";
+    return nameProduct;
+}
+
+function addFirstParagraph(pos){
+    const firstParagraph = document.createElement("p");
+    firstParagraph.textContent = cart[pos].quantity +"x";
+    return firstParagraph;
+}
+
+function addSecondParagraph(id){
+    const secondParagraph = document.createElement("p");
+    secondParagraph.textContent = "@ "+product[id].price;
+    return secondParagraph;
+}
+
+function addTotal(pos, id){
+    const p = document.createElement("b");
+    p.textContent = "$"+cart[pos].quantity*product[id].price;
+    
+    return p;
+}
+const $ = function(element){
+    document.getElementById(element);
+}
+function confirmOrder(){
+    const section = document.getElementsByClassName("ocult-confirm");
+    const article = document.getElementById("food-details");
+    
+    for (let i = 0; i < cart.length; i++) {
+        let div =document.createElement("div");
+        div.appendChild(addImages(i));
+        div.appendChild(divParagraph(cart[i].id, i));
+        div.appendChild(addTotal(i, cart[i].id));
+        div.classList.add("food-detail"); 
+        article.appendChild(div);
+    }
+    
+    
+    for (let i = 0; i < section.length; i++) {
+        section[i].style.display = "block";
+    }
+}
+
+function addImages(i){
+    
+    const img = document.createElement("img");
+    img.src = product[cart[i].id].img;
+    return img;
+}
+function refresh(){
+    location.reload();
 }
